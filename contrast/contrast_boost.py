225216225ce823925820aef94eb8cb3431fc1208
InfinityFalls <anthony2006chiang@outlook.com>
2026-03-25 13:32:36 +0000

    refactor stuff into utils

--- contrast/contrast_boost.py
+++ contrast/contrast_boost.py
@@ -1,55 +1,10 @@
 
 import colour
-import numpy as np
 import sys
 
+from contrast.utils import adjust_c, adjust_y, get_y_from_rgb, rgb_to_string, string_to_rgb
+
 TARGET_Y = 0.34937594007454498
-
-
-def string_to_rgb(hex_str: str):
-    r = int(hex_str[1:3], 16) / 255
-    g = int(hex_str[3:5], 16) / 255
-    b = int(hex_str[5:7], 16) / 255
-    return np.array((r, g, b))
-
-
-def rgb_to_string(rgb):
-    r = hex(round(rgb[0] * 255))[2:].rjust(2, "0")
-    g = hex(round(rgb[1] * 255))[2:].rjust(2, "0")
-    b = hex(round(rgb[2] * 255))[2:].rjust(2, "0")
-    return "#"+r+g+b
-
-
-def adjust_y(oklch, steps):
-    for _ in range(steps):
-        xyz = colour.convert(oklch, "oklch", "cie xyz")
-        xyz[1] = TARGET_Y
-        oklch[0] = colour.convert(xyz, "cie xyz", "oklch")[0]
-        print(xyz[1])
-
-
-def get_y_from_rgb(rgb):
-    xyz = colour.convert(rgb, "srgb", "cie xyz")
-    return xyz[1]
-
-
-def check_oklch(oklch):
-    rgb = colour.convert(oklch, "oklch", "srgb")
-    return np.all((0 <= rgb) & (rgb <= 1))
-
-
-def adjust_c(oklch, steps):
-    l = 0
-    r = 0.32
-    for _ in range(steps):
-        m = (l + r) / 2
-        oklch[1] = m
-        if check_oklch(oklch):
-            l = m
-        else:
-            r = m
-        print(r - l)
-    oklch[1] = l
 
 
 def main():
@@ -57,7 +12,7 @@ def main():
     input_rgb = string_to_rgb(to_boost)
     oklch = colour.convert(input_rgb, "srgb", "oklch")
     for i in range(3):
-        adjust_y(oklch, 100)
+        adjust_y(oklch, 100, TARGET_Y)
         adjust_c(oklch, 100)
     boosted_rgb = colour.convert(oklch, "oklch", "srgb")
     print(rgb_to_string(boosted_rgb))
---
+++ contrast/utils.py
@@ -0,0 +1,49 @@
+import colour
+import numpy as np
+
+
+
+def rgb_to_string(rgb):
+    r = hex(round(rgb[0] * 255))[2:].rjust(2, "0")
+    g = hex(round(rgb[1] * 255))[2:].rjust(2, "0")
+    b = hex(round(rgb[2] * 255))[2:].rjust(2, "0")
+    return "#"+r+g+b
+
+
+def string_to_rgb(hex_str: str):
+    r = int(hex_str[1:3], 16) / 255
+    g = int(hex_str[3:5], 16) / 255
+    b = int(hex_str[5:7], 16) / 255
+    return np.array((r, g, b))
+
+
+def get_y_from_rgb(rgb):
+    xyz = colour.convert(rgb, "srgb", "cie xyz")
+    return xyz[1]
+
+
+def check_oklch(oklch):
+    rgb = colour.convert(oklch, "oklch", "srgb")
+    return np.all((0 <= rgb) & (rgb <= 1))
+
+
+def adjust_c(oklch, steps):
+    l = 0
+    r = 0.32
+    for _ in range(steps):
+        m = (l + r) / 2
+        oklch[1] = m
+        if check_oklch(oklch):
+            l = m
+        else:
+            r = m
+        print(r - l)
+    oklch[1] = l
+
+
+def adjust_y(oklch, steps, target_y):
+    for _ in range(steps):
+        xyz = colour.convert(oklch, "oklch", "cie xyz")
+        xyz[1] = target_y
+        oklch[0] = colour.convert(xyz, "cie xyz", "oklch")[0]
+        print(xyz[1])
